1e1189a45e11ff356439fbd7dc8cdbddbcde7e07
Hnaa17 <farhanaachmad8578@gmail.com>
2022-11-23 22:41:50 +0700

    first commit

---
+++ query.sql
@@ -0,0 +1,35 @@
+create table users (
+    id varchar primary key,
+    name varchar(50),
+    email varchar(50),
+    password varchar,
+    is_verified boolean,
+    token varchar,
+    photo varchar,
+    shortname varchar(20),
+    bio varchar(255),
+    phone varchar(15),
+    created_at date,
+    date date
+);
+
+create table grup(
+    id varchar primary key,
+    name_grup varchar(50),
+    created_at date,
+    date date,
+    photo varchar,
+    sender varchar,
+    message varchar
+);
+
+create table messages(
+    id VARCHAR primary key, 
+    sender_id varchar not null,
+    receiver_id varchar not null,
+    message varchar,
+    created_at date,
+    date date,
+    sender varchar,
+    photo varchar
+);
